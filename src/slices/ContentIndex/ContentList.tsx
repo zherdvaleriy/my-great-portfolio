aaf9e25672c209781da7b8961f56b07384d2e4f0
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-03-27 13:56:10 +0100

    modified ContentList ant index.tsx in ContentIndex

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -14,9 +14,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
-  contentType: Content.BlogPostIndexSlice["primary"]["content_type"];
-  fallbackItemImage: Content.BlogPostIndexSlice["primary"]["fallback_item_image"];
-  viewMoreText: Content.BlogPostIndexSlice["primary"]["view_more_text"];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
 };
 
 export default function ContentList({
--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -1,3 +1,53 @@
+// import { Content, isFilled } from "@prismicio/client";
+// import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+// import { createClient } from "@/prismicio";
+// import ContentList from "./ContentList";
+// import Bounded from "@/components/Bounded";
+// import Heading from "@/components/Heading";
+// /**
+//  * Props for `BlogPostIndex`.
+//  */
+// export type BlogPostIndexProps =
+//   SliceComponentProps<Content.BlogPostIndexSlice>;
+
+// /**
+//  * Component for "BlogPostIndex" Slices.
+//  */
+// const BlogPostIndex = async ({
+//   slice,
+// }: BlogPostIndexProps): Promise<JSX.Element> => {
+//   const client = createClient();
+//   const blogPosts = await client.getAllByType("blog_post");
+//   const projects = await client.getAllByType("project");
+
+//   const items = slice.primary.content_type === "Blog" ? blogPosts : projects;
+
+//   return (
+//     <Bounded
+//       data-slice-type={slice.slice_type}
+//       data-slice-variation={slice.variation}
+//     >
+//       <Heading size="xl" className="mb-8">
+//         {slice.primary.heading}
+//       </Heading>
+//       {isFilled.richText(slice.primary.description) && (
+//         <div className="prose prose-xl prose-invert mb-10">
+//           <PrismicRichText field={slice.primary.description} />
+//         </div>
+//       )}
+//       <ContentList
+//         items={items}
+//         contentType={slice.primary.content_type}
+//         viewMoreText={slice.primary.view_more_text}
+//         fallbackItemImage={slice.primary.fallback_item_image}
+//       />
+//     </Bounded>
+//   );
+// };
+
+// export default BlogPostIndex;
+
+
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { createClient } from "@/prismicio";
@@ -7,15 +57,15 @@ import Heading from "@/components/Heading";
 /**
  * Props for `BlogPostIndex`.
  */
-export type BlogPostIndexProps =
-  SliceComponentProps<Content.BlogPostIndexSlice>;
+export type ContentIndexProps =
+  SliceComponentProps<Content.ContentIndexSlice>;
 
 /**
  * Component for "BlogPostIndex" Slices.
  */
-const BlogPostIndex = async ({
+const ContentIndex = async ({
   slice,
-}: BlogPostIndexProps): Promise<JSX.Element> => {
+}: ContentIndexProps): Promise<JSX.Element> => {
   const client = createClient();
   const blogPosts = await client.getAllByType("blog_post");
   const projects = await client.getAllByType("project");
@@ -45,4 +95,4 @@ const BlogPostIndex = async ({
   );
 };
 
-export default BlogPostIndex;
+export default ContentIndex;
