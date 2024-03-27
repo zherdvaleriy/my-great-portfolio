d8c6f86ea9dd4550193dcc2522945c74f0fade8b
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-03-27 13:36:40 +0100

    changed index.tsx in ContextIndex

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -1,50 +1,48 @@
-import Heading from "@/components/Heading";
 import { Content, isFilled } from "@prismicio/client";
-import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
-import Bounded from '../../components/Bounded';
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { createClient } from "@/prismicio";
 import ContentList from "./ContentList";
-import { createClient } from '@/prismicio';
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+/**
+ * Props for `BlogPostIndex`.
+ */
+export type BlogPostIndexProps =
+  SliceComponentProps<Content.BlogPostIndexSlice>;
 
 /**
- * Props for `ContentIndex`.
+ * Component for "BlogPostIndex" Slices.
  */
-export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
+const BlogPostIndex = async ({
+  slice,
+}: BlogPostIndexProps): Promise<JSX.Element> => {
+  const client = createClient();
+  const blogPosts = await client.getAllByType("blog_post");
+  const projects = await client.getAllByType("project");
 
-/**
- * Component for "ContentIndex" Slices.
- */
-const ContentIndex = async({ slice }: ContentIndexProps): Promise<JSX.Element> => {
-  
-  const client = createClient()
-  const blogPosts = await client.getAllByType('blog_post')
-  const projects = await client.getAllByType('project')
-
-  const contentType = slice.primary.content_type || 'Blog'
-
-  const items = contentType === 'Blog' ? blogPosts : projects
+  const items = slice.primary.content_type === "Blogs" ? blogPosts : projects;
 
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-     <Heading size="xl" className="mb-8" >
-       {slice.primary.heading}
-     </Heading>
+      <Heading size="xl" className="mb-8">
+        {slice.primary.heading}
+      </Heading>
       {isFilled.richText(slice.primary.description) && (
         <div className="prose prose-xl prose-invert mb-10">
           <PrismicRichText field={slice.primary.description} />
         </div>
       )}
-
-      <ContentList 
-          items={items} 
-          contentType={contentType} 
-          viewMoreText={slice.primary.view_more_text}
-          fallbackItemImage={slice.primary.fallback_item_image}
-          />
+      <ContentList
+        items={items}
+        contentType={slice.primary.content_type}
+        viewMoreText={slice.primary.view_more_text}
+        fallbackItemImage={slice.primary.fallback_item_image}
+      />
     </Bounded>
   );
 };
 
-export default ContentIndex;
+export default BlogPostIndex;
