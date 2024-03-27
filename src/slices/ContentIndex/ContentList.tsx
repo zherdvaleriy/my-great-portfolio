7c94c1246b6c52d163e345abaf30a6f30d48a1a9
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-03-27 14:47:59 +0100

    ref change

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -151,7 +151,7 @@ export default function ContentList({
         {items.map((post, index) => (
           <li
             key={index}
-            ref={(el) => (itemsRef.current[index] = el)}
+            ref={(el) => {itemsRef.current[index] = el}}
             onMouseEnter={() => onMouseEnter(index)}
             className="list-item opacity-0"
           >
