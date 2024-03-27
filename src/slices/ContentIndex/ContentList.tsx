be377b40b738591bdfc6844d6febca2a27a263c1
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2024-03-27 14:01:06 +0100

    changed for 'Blog'

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -33,7 +33,7 @@ export default function ContentList({
   const [hovering, setHovering] = useState(false);
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blogs" ? "/blog" : "/project";
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/project";
 
   useEffect(() => {
     // Animate list-items in with a stagger
